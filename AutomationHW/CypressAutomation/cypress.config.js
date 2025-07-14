04de3e92d77ccc201c6c1d0bcdf89f0bd4755fd0
Engin Deniz GÜN <69649412+Osprey07@users.noreply.github.com>
2025-07-14 11:02:07 +0300

    v1

--- AutomationHW/CypressAutomation/cypress.config.js
+++ AutomationHW/CypressAutomation/cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config); // Allure'u etkinleştir
+      return config;
     },
+    baseUrl: 'https://magento.softwaretestingboard.com/', // kendi baseUrl'in neyse
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js'
   },
 });
